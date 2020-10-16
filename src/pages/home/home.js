6841b6b2d180db1ecf41863fbe762e0f96106518
Edward Ramos <edward.ramos.guimaraes@gmail.com>
2020-10-15 22:07:05 -0300

    api

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,46 @@
 import React, {Component} from 'react';
+import "./home.css"
+
+import pikachu from "../../img/pikachu.png";
+import bulbasaur from "../../img/bulbasaur.png";
+import squirtle from "../../img/squirtle.png";
+import charmander from "../../img/charmander.png";
 
 class Home extends Component {
     render(){
         return (
-            <div>Este é o Home</div>
+            <div className="home pb-5">
+                <p className="titulo">Pokemon</p>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-3">
+                            <img className="photo" src={pikachu}></img>
+                        </div>
+                        <div className="col-3">
+                            <img className="photo" src={bulbasaur}></img>
+                        </div>
+                        <div className="col-3">
+                            <img className="photo" src={squirtle}></img>
+                        </div>
+                        <div className="col-3">
+                            <img className="photo" src={charmander}></img>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-5 mr-5 text-center borda">
+                            Infos 1
+                            <p>Texto 1</p>
+                            <p>Texto</p>
+                            <p>Texto</p>
+                            <p>Texto</p>
+                            <p>Texto</p>
+                        </div>
+                        <div className="col-5 ml-5 text-center borda">
+                            Infos 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
+    constructor(){
+        super();
+        this.setState({ data: {}});
+    }
+
+    componentDidMount(){
+        fetch('https://api.github.com/users/kalbzero/repos')
+        .then( (response) => {response.json()} )
+        .then( (data) => { this.setState( {data: data} ) } )
+    }
+    
     render(){
         return (
-            <div>Este é o Login!</div>
+        <div>Nome de usuário: {this.state.data}</div>
         );
     }
 }
